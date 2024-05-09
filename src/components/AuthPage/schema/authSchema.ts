12bd935f3afbc81eb3b4be231c1e0192184e08d9
asandan <asan.dan.2121@mail.ru>
2024-05-10 01:17:40 +0500

    Implement MedicalCertifications form

--- src/components/AuthPage/schema/authSchema.ts
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-
-const authSchema = yup.object().shape({
-  email: yup.string().email("Please enter a valid email address").required("Email is required"),
-  password: yup.string().min(6, "Min length is 6 symbols").required("Password is required"),
-});
-
-export default authSchema
